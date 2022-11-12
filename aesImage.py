b1af01cce16c2782c8319d3e21f80e20e8f0d157
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-11-12 13:42:22 -0600

    encrypts files and asks for name

--- aesImage.py
+++ aesImage.py
@@ -1,31 +1,34 @@
 import time
 from cryptography.fernet import Fernet
 
-#starting timer
-start = time.perf_counter()
-
 key = Fernet.generate_key()
 f = Fernet(key)
 
+fileName = input("Please enter path of desired file: ")
+fileExt = input("Enter file extension without a .  ie: jpeg ")
+
+#starting timer
+start = time.perf_counter()
+
 #opening image and assigning bytes to token var
-with open('/Users/emmanuelhuitron/Desktop/Test/motorCycle.cr2', 'rb') as originalFile:
+with open(fileName, 'rb') as originalFile:
     token = originalFile.read()
 
 encrypted = f.encrypt(token)
 
 #writing encrypted image file
-with open ('/Users/emmanuelhuitron/Desktop/Test/encImage.cr2', 'wb') as encryptedFile:
+with open ('/Users/emmanuelhuitron/Desktop/Test/encFile.txt', 'wb') as encryptedFile:
     encryptedFile.write(encrypted)
 
 middle = time.perf_counter()
 print(f'Finished encryption in {round(middle-start, 2)} second(s)')
 
-with open('/Users/emmanuelhuitron/Desktop/Test/encImage.cr2', 'rb') as encryptedFile:
+with open('/Users/emmanuelhuitron/Desktop/Test/encFile.txt', 'rb') as encryptedFile:
     encrypted = encryptedFile.read()
 
 decrypted = f.decrypt(encrypted)
 
-with open('/Users/emmanuelhuitron/Desktop/Test/decImage.cr2', 'wb') as decryptedFile:
+with open('/Users/emmanuelhuitron/Desktop/Test/decImages.' + fileExt, 'wb') as decryptedFile:
     decryptedFile.write(decrypted)
 
 finish = time.perf_counter()
