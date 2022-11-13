e9cd1632666c9385af3521a282cf3549989144a6
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-11-13 17:48:05 -0600

    added time counter to camelliaImage

--- camelliaImage.py
+++ camelliaImage.py
@@ -11,6 +11,9 @@ decFilePath = "/Users/emmanuelhuitron/Desktop/Decrypted"
 c1 = camellia.CamelliaCipher(key=b'16 byte long key', IV=b'16 byte iv. abcd', mode=camellia.MODE_CBC)
 c2 = camellia.CamelliaCipher(key=b'16 byte long key', IV=b'16 byte iv. abcd', mode=camellia.MODE_CBC)
 
+#starting timer
+start = time.perf_counter()
+
 #opening image and assigning bytes to the var "token"
 with open(filesPath + "/" + dirList[0], 'rb') as originalFile:
     token = originalFile.read()
@@ -22,6 +25,8 @@ with open(encFilePath + "/" + dirList[0], 'wb') as encryptedFile:
     encryptedFile.write(encrypted)
 print("Encrypted the file.")
 
+middle = time.perf_counter()
+
 #opening encrypted image
 with open(encFilePath + "/" + dirList[0], 'rb') as encryptedFile:
     encrypted = encryptedFile.read()
@@ -31,3 +36,8 @@ decrypted = c2.decrypt(encrypted)
 with open(decFilePath + "/" + dirList[0], 'wb') as decryptedFile:
     decryptedFile.write(decrypted)
 print("Decrypted the file.")
+
+finish = time.perf_counter()
+print(f'***Finished encryption in {round(middle-start, 2)} second(s)***')
+print(f'***Finished decryption in {round(finish-middle, 2)} second(s)***')
+print(f'***Finished overall in {round(finish-start, 2)} second(s)***')
