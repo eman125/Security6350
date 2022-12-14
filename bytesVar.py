1107cf0bbf84f51572b82a1c5af70aac58b8f9bc
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-12-14 12:45:58 -0600

    deleted unused files

--- bytesVar.py
+++
@@ -1,9 +0,0 @@
-import os
-
-filePath = input("Please enter file path: ")
-
-with open(filePath, 'rb') as originalFile:
-    token = originalFile.read()
-
-print(type(token))
-print("token: ", token)
--- directoryTest.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = input("Please ender directory: ")
-dirList = os.listdir(path)
-
-i = 1
-print("deleting second file which is: ", dirList[1])
-dirList.remove(dirList[i])
-print("dirList is now: ", dirList)
