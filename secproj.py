3857d23ed55488ba11176aca36044c6e7b9bbf8c
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-12-29 16:17:29 -0600

    fixed case typo in main

--- secproj.py
+++ secproj.py
@@ -1,11 +1,12 @@
 import typer
-import aesTest
+import aestest
 
 app = typer.Typer()
 
 @app.command()
 def main(alg: str, filepath: str):
     print(f"Running in secproj; alg: {alg} filepath: {filepath}")
+    aestest.aes_func(filepath)
 
 if __name__ == "__main__":
     app()
