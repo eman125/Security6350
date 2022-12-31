c9b9ba2c1ec809d0dc106b4d282b93f5b1ff13f7
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-12-31 12:32:29 -0600

    starting to add encryption portion

--- aestest.py
+++ aestest.py
@@ -1,5 +1,6 @@
 import time
 import os
+import rmdot
 from cryptography.fernet import Fernet
 
 def aes_func(filePath, encFilePath, decFilePath):
@@ -9,3 +10,28 @@ def aes_func(filePath, encFilePath, decFilePath):
     print("AES key: ", key)
 
     dirList = os.listdir(filePath)
+
+    #remove hidden files from array that should not be encrypted
+    dirList = rmdot.rm_dot(dirList)
+
+    #creating start timestamp
+    start = time.perf_counter()
+
+    i = 0
+    while i < len(dirList):
+        #opening image and assigning bytes to token var
+        with open(filePath + "/" + dirList[i], 'rb') as originalFile:
+            token = originalFile.read()
+
+        encrypted = f.encrypt(token)
+
+        #writing encrypted image file
+        with open(encFilePath + "/" + dirList[i], 'wb') as encryptedFile:
+            encryptedFile.write(encrypted)
+        print("Encrypted file ", i)
+        i += 1
+
+    middle = time.perf_counter()
+    print("\nFinished encryption.\n")
+
+
