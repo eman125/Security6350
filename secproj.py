444a533e6b68e97a5d650cf8bc124284a5bd38fb
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-12-30 18:58:21 -0600

    made aes and camellia commands

--- secproj.py
+++ secproj.py
@@ -4,9 +4,13 @@ import aestest
 app = typer.Typer()
 
 @app.command()
-def main(alg: str, filepath: str):
-    print(f"Running in secproj; alg: {alg} filepath: {filepath}")
+def aes(filepath: str, custom_path: bool = False):
+    print(f"Running in secproj; filepath: {filepath} Is custom: {custom_path}")
     aestest.aes_func(filepath)
+
+@app.command()
+def camellia(filepath: str, custom_path: bool = False):
+    print(f"Running in secproj; filepath: {filepath} Is custom: {custom_path}")
 
 if __name__ == "__main__":
     app()
