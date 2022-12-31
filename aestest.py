73634d9218e74df4baa713ba1fe47e14b9439f01
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-12-30 19:02:55 -0600

    Added lines to aestest.py

--- aestest.py
+++ aestest.py
@@ -2,5 +2,10 @@ import time
 import os
 from cryptography.fernet import Fernet
 
-def aes_func(filePath):
+def aes_func(filePath, encFilePath):
     print(f"passed file path: {filePath}")
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    print("AES key: ", key)
+
+    dirList = os.listdir(filesPath)
